956bc07e9ccff8b7118d71826158422feb2def3e
wangyigui <466007639@qq.com>
2017-04-11 19:55:45 +0800

    '1'

---
+++ src/AppendDemo.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Snoopy on 2017/4/11.
+ */
+public class AppendDemo {
+    public static void main(String[] args) {
+//        StringBuffer sb = new StringBuffer();
+//        sb.ensureCapacity(2);
+//        sb.append(2.2f);
+//        sb.append(123);
+//        sb.append(true);
+//        sb.insert(2,123);
+//        sb.delete(1,4);
+//        System.out.println(sb);
+//        System.out.println(sb.capacity());
+//        System.out.println(System.currentTimeMillis());
+//
+        System.out.println(System.getenv("path"));
+    }
+}
