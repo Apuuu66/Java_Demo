26a254760b00435ecf3fb2d2c7407d2f04e4863e
wangyigui <466007639@qq.com>
2017-05-31 21:57:59 +0800

    '正则匹配'

---
+++ src/May31/Dregex.java
@@ -0,0 +1,19 @@
+package May31;
+
+import java.util.Arrays;
+
+/**
+ * Created by Snoopy on 2017/5/31.
+ */
+public class Dregex {
+    public static void main(String[] args) {
+        String str="我喔要要要要学学学学学学刚好嘻嘻嘻嘻嘻嘻";
+        String [] date=str.split("(.)\\1+");
+        System.out.println(Arrays.toString(date));
+//        for (int i = 0; i <date.length ; i++) {
+//            System.out.println(date[i]);
+//        }
+        str = str.replaceAll("(.)\\1+","$1");
+        System.out.println(str);
+    }
+}
